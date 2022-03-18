d3ceb5e4ea5a9129a622d45d6ac6797ebafe0b8b
LaerkeSoeby <laer9384@edu.zealand.dk>
2022-03-18 13:40:56 +0100

    Filter startet, javascript + html + css

--- script.js
+++ script.js
@@ -3,6 +3,28 @@ function openMenu() {
     if (x.style.display === "flex") {
         x.style.display = "none";
     } else {
-      x.style.display = "flex";
+        x.style.display = "flex";
     }
-  }
+}
+
+function filterOutdoor() {
+    hideFilters()
+}
+
+function filterEvents() {
+    hideFilters()
+}
+
+function filterIndoor() {
+    hideFilters()
+}
+
+function filterBarsel() {
+    hideFilters()
+}
+
+function hideFilters() {
+    let loungeBanner = document.getElementById("lounge-banner");
+    let udvalgteSection = document.getElementById("udvalgte-section");
+
+}
